1c450d83d0306d335ad1002ebdc16483b99eb867
Petr Jasek <jasekpetr@gmail.com>
2024-10-02 10:42:21 +0200

    save

--- server/planning/commands/delete_marked_assignments_test.py
+++ server/planning/commands/delete_marked_assignments_test.py
@@ -8,13 +8,16 @@
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
 
+import flask
+
 from .delete_marked_assignments import DeleteMarkedAssignments
 from planning.tests import TestCase
 from superdesk import get_resource_service
 from superdesk.utc import utcnow
 from datetime import timedelta
 from bson import ObjectId
-import flask
+from unittest.mock import patch
+
 
 now = utcnow()
 yesterday = now - timedelta(hours=48)
@@ -30,9 +33,9 @@ class DeleteMarkedAssignmentsTest(TestCase):
     ]
 
     assignments = [
-        {"_id": "a1", "_to_delete": True, "planning_item": "p1", "coverage_item": "c1"},
-        {"_id": "a2", "_to_delete": True, "planning_item": "p2", "coverage_item": "c1"},
-        {"_id": "a3", "planning_item": "p3"},
+        {"_id": ObjectId(), "_to_delete": True, "planning_item": "p1", "coverage_item": "c1"},
+        {"_id": ObjectId(), "_to_delete": True, "planning_item": "p2", "coverage_item": "c1"},
+        {"_id": ObjectId(), "planning_item": "p3"},
     ]
     plans = [{"_id": "p1"}, {"_id": "p2"}, {"_id": "p3"}]
 
@@ -40,8 +43,9 @@ class DeleteMarkedAssignmentsTest(TestCase):
         super().setUp()
         self.assignment_service = get_resource_service("assignments")
 
-    def assertAssignmentDeleted(self, assignment_ids, not_deleted=False):
-        for assignment_id in assignment_ids:
+    def assertAssignmentDeleted(self, assignment_indexes, not_deleted=False):
+        for assignment_index in assignment_indexes:
+            assignment_id = self.assignments[assignment_index]["_id"]
             assignment = self.assignment_service.find_one(_id=assignment_id, req=None)
             if not_deleted:
                 self.assertIsNotNone(assignment)
@@ -58,7 +62,14 @@ class DeleteMarkedAssignmentsTest(TestCase):
             flask.g.user = self.users[0]
             flask.g.auth = self.auth[0]
 
-            DeleteMarkedAssignments().run()
 
-            self.assertAssignmentDeleted(["a1", "a2"])
-            self.assertAssignmentDeleted(["a3"], True)
+            with patch("planning.commands.delete_marked_assignments.push_notification") as notification_mock:
+                DeleteMarkedAssignments().run()
+                notification_mock.assert_called_once()
+
+            self.assertAssignmentDeleted([0, 1])
+            self.assertAssignmentDeleted([2], True)
+
+            with patch("planning.commands.delete_marked_assignments.push_notification") as notification_mock:
+                DeleteMarkedAssignments().run()
+                notification_mock.assert_not_called()
